13b4ff1f2244f0225c14b53e4eb2facc4d7fc2f3
Vlokesh01 <vepamanulokesh01@gmail.com>
2025-11-02 13:04:28 +0530

    solved mongodb error

--- src/config/db.js
+++ src/config/db.js
@@ -6,9 +6,6 @@ const MONGO_URI = process.env.MONGO_URI;
 
 export default async function connectDB() {
   if (!MONGO_URI) throw new Error('MONGO_URI not set in env');
-  await mongoose.connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  await mongoose.connect(MONGO_URI);
   console.log('MongoDB connected');
 }
